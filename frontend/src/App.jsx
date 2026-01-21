8c041b7deb0c27c8f276bd2285706c1c0b0607de
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-01-21 21:28:50 +0300

    edited

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Properties from './pages/Properties'
+import PropertyDetails from './pages/PropertyDetails'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import PrivateRoute from './components/PrivateRoute'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="properties" element={<Properties />} />
+        <Route path="properties/:id" element={<PropertyDetails />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        
+        {/* Protected Routes */}
+        <Route path="dashboard/*" element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        } />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
